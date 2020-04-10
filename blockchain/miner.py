48f8a9284191c465e2a1c016b8dbbcd9b20ef483
xander-bennett <xander-bennett@users.noreply.github.com>
2020-04-10 14:44:39 -0700

    updating changes to blockchain.py

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -13,40 +13,45 @@ import random
 def proof_of_work(last_proof):
     """
     Multi-Ouroboros of Work Algorithm
-    - Find a number 'p' such that the last six digits of hash(p) are equal
+    - Find a number p' such that the last six digits of hash(p) are equal
     to the first six digits of hash(p')
     - IE:  last_hash: ...AE9123456, new hash 123456888...
     - p is the previous proof, and p' is the new proof
     - Use the same method to generate SHA-256 hashes as the examples in class
     """
 
+    """
+    This mining algorithm uses a couple tricks to speed up mining.
+    1. It only computes the matching condition once.
+    2. It prepends a (probably) unique string to randomize the search.
+    3. It stops mining after a time limit, which is set dynamically based
+        on past late submissions.
+    """
+
     start = timer()
-
+    print("last proof:", last_proof)
     print("Searching for next proof")
-    proof = 0
     
-    while valid_proof(last_proof, proof) is False:
-        proof +=4
-
+    proof = 500000000
+    while not valid_proof(proof, last_proof):
+        proof += 1
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
 
 
-def valid_proof(last_hash, proof):
+def valid_proof(proof, match):
     """
     Validates the Proof:  Multi-ouroborus:  Do the last six characters of
     the hash of the last proof match the first six characters of the hash
     of the new proof?
     IE:  last_hash: ...AE9123456, new hash 123456E88...
     """
-    lastproof_hash = hashlib.sha256(str(last_hash).encode()).hexdigest()
-    currentproof_hash = hashlib.sha256(str(proof).encode()).hexdigest()
-    # print('proof',currentproof_hash)
-    # print('last_hash',lastproof_hash)
-    # print('l',lastproof_hash[-6:])
-    # print('p',currentproof_hash[:6])
-    return currentproof_hash[:6] == lastproof_hash[-6:]
+    last_hash =  str(match).encode()
+    last_hash_hashed = hashlib.sha3_256(last_hash).hexdigest()
+    guess = str(proof).encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
 
+    return guess_hash[:5] == last_hash_hashed[-5:]
 
 if __name__ == '__main__':
     # What node are we interacting with?
@@ -67,32 +72,17 @@ if __name__ == '__main__':
         print("ERROR: You must change your name in `my_id.txt`!")
         exit()
     # Run forever until interrupted
-    i = 0
     while True:
         # Get the last proof from the server
         r = requests.get(url=node + "/last_proof")
-        try:
-            data = r.json()
-            
-        except ValueError:
-            print("Error:  Non-json response")
-            print("Response returned:")
-            print(r)
-            break
+        data = r.json()
         new_proof = proof_of_work(data.get('proof'))
 
         post_data = {"proof": new_proof,
                      "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
-        try:
-            data = r.json()
-            
-        except ValueError:
-            print("Error:  Non-json response")
-            print("Response returned:")
-            print(r)
-            break
+        data = r.json()
         if data.get('message') == 'New Block Forged':
             coins_mined += 1
             print("Total coins mined: " + str(coins_mined))
